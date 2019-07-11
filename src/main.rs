2360858c12b803d1e2d2b6728c1aa962c70a06c4
darshimo <takumishimoda7623@gmail.com>
2019-07-12 06:27:38 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,35 @@
+mod generator;
 mod lambda;
+mod lambda_double;
 mod lambda_mu;
-//mod lambdabar_mu;
+mod lambdabar_mu;
 mod lambdabar_mu_mutilde;
+mod lambdabar_mu_mutilde_comp;
+mod lexer;
 mod read;
 
-use crate::lambda::RedType::{CBN, CBVL, CBVR};
-use std::collections::VecDeque;
-
 use lambda::*;
+use lambda_double::*;
 use lambda_mu::*;
-//use lambdabar_mu::*;
+use lambdabar_mu::*;
 use lambdabar_mu_mutilde::*;
-
-enum Calculus {
-    L,
-    LM,
-    LbMMt,
-}
-
-use Calculus::{LbMMt, L, LM};
+use lambdabar_mu_mutilde_comp::*;
 
 fn main() {
-    //println!("head reduction machine in λ̅μ-calculus");
-    //println!("please input λ̅μ-term");
+    let s: String = read::read();
 
-    println!("please input term to reduce.");
+    let e = LMCommand::new(s);
+    let e1 = LbMMtCompCommand::translate_l2r(&e);
+    let e1r = e1.reverse();
+    println!("e  : {}", e);
+    println!("e1 : {}", e1);
+    println!("e1r: {}", e1r);
 
-    let (s, clcls) = remove_head_whitespace(&read::read());
+    let e2 = LTerm::translate_command_cbn(&e1);
+    println!("e2");
+    e2.reduction(&lambda::RedType::CBN);
 
-    println!("");
-    println!("{}", s);
-    println!("");
-
-    match clcls {
-        L => {
-            println!("head reduction in λ-calculus");
-            println!("select CBN(0) or CBVL(1) or CBVR(2)");
-            let i: u8 = read::read();
-            let rt: RedType;
-            if i == 0 {
-                rt = CBN;
-            } else if i == 1 {
-                rt = CBVL;
-            } else if i == 2 {
-                rt = CBVR;
-            } else {
-                panic!("error");
-            }
-            let e = LTerm::new(s);
-            println!("");
-            e.reduction(&rt);
-        }
-        LM => {
-            println!("head reduction in λμ-calculus");
-            let e = LMCommand::new(s);
-            println!("");
-            e.reduction();
-        }
-        LbMMt => {
-            println!("head reduction in λ̅μμ̃-calculus");
-            println!("select CBN(0) or CBV(1)");
-            let i: u8 = read::read();
-            let rt: bool;
-            if i == 0 {
-                rt = true;
-            } else if i == 1 {
-                rt = false;
-            } else {
-                panic!("error");
-            }
-            let e = LbMMtCommand::new(s);
-            println!("");
-            e.reduction(rt);
-        }
-    }
-}
-
-fn remove_head_whitespace(s: &String) -> (String, Calculus) {
-    let mut vd: VecDeque<char> = s.chars().collect();
-    let clcls: Calculus;
-    loop {
-        let c = vd.get(0).expect("empty string.");
-        if *c == ' ' {
-            vd.pop_front();
-        } else {
-            if *c == '[' {
-                clcls = LM;
-            } else if *c == '⟨' {
-                clcls = LbMMt;
-            } else {
-                clcls = L;
-            }
-            break;
-        }
-    }
-    (vd.into_iter().collect::<String>(), clcls)
+    let e3 = LTerm::translate_command_cbv(&e1);
+    println!("e3");
+    e3.reduction(&lambda::RedType::CBN);
 }
