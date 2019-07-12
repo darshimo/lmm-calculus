3ad38f14c11936e60787505dfc68cba57bb8af18
darshimo <takumishimoda7623@gmail.com>
2019-07-12 11:21:38 +0900

    reverse variable name

--- src/variable.rs
+++ src/variable.rs
@@ -59,3 +59,26 @@ pub fn generate_kvariable() -> String {
 
   format!("k{}", s)
 }
+
+pub fn reverse_free_variable(s: &String) -> String {
+  if s.contains("̅") {
+    let mut s1 = s.clone();
+    s1.retain(|c| c != '̅');
+    s1
+  } else {
+    let mut cv: Vec<char> = vec![];
+    for c in s.chars() {
+      cv.push(c);
+      cv.push('̅');
+    }
+    cv.into_iter().collect()
+  }
+}
+
+pub fn reverse_bound_variable(s: &String) -> String {
+  if s.contains("x") {
+    s.replace("x", "α")
+  } else {
+    s.replace("α", "x")
+  }
+}
